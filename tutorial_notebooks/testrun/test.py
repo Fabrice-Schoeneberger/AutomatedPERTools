6d6b441508c7c979740f38ad84568bf483e31a50
Fabrice Schöneberger <fabrice.schoeneberger@googlemail.com>
2024-11-14 01:51:26 +0100

    Added support for real QPUs

--- tutorial_notebooks/testrun/test.py
+++ tutorial_notebooks/testrun/test.py
@@ -1,18 +1,9 @@
-import argparse
-import os
-parser = argparse.ArgumentParser()
-parser.add_argument('--circuitfilename', type=str, help='Set the name of the file, that contains the function making you circuit', default="circuits")
-parser.add_argument('--circuitfunction', type=str, help='Set the name of the function, that return your circuits. The function can only take a backend as input and has to return an array of circuits', default="test")
-args = parser.parse_args()
+# Prompt the user to type "yes" to continue
+response = input("Type 'yes' to continue: ")
 
-if args.circuitfilename.endswith(".py"):
-    circuitfilename = args.circuitfilename[:-3]
-else:
-    circuitfilename = args.circuitfilename
-import importlib.util
-spec = importlib.util.spec_from_file_location(circuitfilename, f"{(circuitfilename)}.py", )
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-get_circuit = getattr(module, args.circuitfunction)
-circuits = get_circuit("backend")
-    
+# Keep prompting until the user types "yes"
+while response.lower() != "yes":
+    response = input("Please type 'yes' to continue: ")
+
+print("Continuing with the program...")
+# Your code here
